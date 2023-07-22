71c168794e09d2c79c721a521e3111183c91eb3c
Crynowecer0 <crynowecer11@gmail.com>
2023-07-22 16:54:10 -0400

    add DLL push

--- dsa-arrays-linked-lists/doubly-linked-list.js
+++ dsa-arrays-linked-lists/doubly-linked-list.js
@@ -48,14 +48,38 @@ class DoublyLinkedList {
    * what cases do we need to handle
    *
    * handle empty list -> need to update head/tail
+   * create a new node
+   * set head to new node
+   * set tail to new node
+   * single node, pointers are all null
    *
-   * handle all other cases
+   * handle all other cases -> simply update tail
+   * access the current tail
+   *
+   * set the current tails next property to the new node
+   * set the new nodes prev to the current tail
+   *
+   * set the DLLs tail to the new node
    *
    *
    */
 
   push(val) {
+    const newNode = new Node(val);
 
+    //handle case of empty list
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+      return;
+    }
+
+    //handle other cases
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+    this.length++;
   }
 
   /** unshift(val): add new value to start of list. */
